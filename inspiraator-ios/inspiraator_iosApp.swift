432d143e6885716772d02e17664c00fd41628e51
Anti Veeranna <anti.veeranna@outlook.com>
2022-02-11 11:33:26 +0200

    Initial Commit

---
+++ inspiraator-ios/inspiraator_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  inspiraator_iosApp.swift
+//  inspiraator-ios
+//
+//  Created by Anti on 11.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct inspiraator_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
